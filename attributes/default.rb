58169ae404233171b0136a6b94bb46e36403e092
Eric Herot <eric.github@herot.com>
2016-04-20 17:47:41 -0400

    Make REPORT_EMAIL an attribute (and set to "vagrant" on TK)

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 default['forensic_reporter']['env']['LOAD_THRESHOLD'] = '0.90'
 default['forensic_reporter']['env']['REPORT_DELAY_SECS'] = 30
 default['forensic_reporter']['env']['OUTPUT_DIR'] = '/tmp/forensic_reporter'
+default['forensic_reporter']['env']['REPORT_EMAIL'] = 'root'
