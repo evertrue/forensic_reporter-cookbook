8bb9fdbc5df794bcb237d7f8af10d4e356e66ec4
Eric Herot <eric.github@herot.com>
2016-04-20 17:47:01 -0400

    Send reports as a bunch of attachments instead of a giant email body

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 default['forensic_reporter']['env']['LOAD_THRESHOLD'] = '0.90'
 default['forensic_reporter']['env']['REPORT_DELAY_SECS'] = 30
-default['forensic_reporter']['env']['OUTPUT_FILE'] = '/tmp/forensic_reporter.tmp'
+default['forensic_reporter']['env']['OUTPUT_DIR'] = '/tmp/forensic_reporter'
